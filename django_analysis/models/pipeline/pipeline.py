0dc3f4d8c78953119e145d22b98f0f7d8875a637
Zvi Baratz <z.baratz@gmail.com>
2019-11-18 11:14:35 +0200

    Refactored pipeline execution logic to the PipelineRunner class

--- django_analysis/models/pipeline/pipeline.py
+++ django_analysis/models/pipeline/pipeline.py
@@ -15,30 +15,6 @@ class Pipeline(TitleDescriptionModel, TimeStampedModel):
     def get_entry_nodes(self) -> Node:
         return [node for node in self.node_set if node.required_nodes is None]
 
-    def run(self, inputs: dict):
-        runs = {node: None for node in self.node_set}
-        for node in self.entry_nodes:
-            node_inputs = inputs.get(node, inputs)
-            runs[node] = node.run(node_inputs)
-        while not all(runs.values()):
-            pending = {key: value for key, value in runs.items() if not value}
-            for node in pending:
-                required_runs = [
-                    runs[required_node] for required_node in node.required_nodes
-                ]
-                if all(required_runs):
-                    input_pipes = self.pipe_set.filter(destination=node)
-                    node_inputs = {
-                        pipe.destination_port.key: [
-                            output.value
-                            for output in runs[pipe.source].output_set
-                            if output.definition.key == pipe.source_port.key
-                        ][0]
-                        for pipe in input_pipes
-                    }
-                    runs[node] = node.run(node_inputs)
-        return runs
-
     @property
     def node_set(self) -> QuerySet:
         return self.get_node_set()
