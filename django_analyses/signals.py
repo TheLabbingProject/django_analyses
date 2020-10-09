8a4888c7015870424907b1d7f0ee39524e8e05f7
Zvi Baratz <z.baratz@gmail.com>
2020-10-09 13:39:05 +0300

    Created a signal to associate a created TaskResult instance with the matching Run instance.

--- django_analyses/signals.py
+++ django_analyses/signals.py
@@ -12,9 +12,10 @@ References
 import shutil
 
 from django.db.models import Model
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
 from django_analyses.models.run import Run
+from django_celery_results.models import TaskResult
 
 
 @receiver(pre_delete, sender=Run)
@@ -23,3 +24,15 @@ def run_pre_delete_receiver(
 ) -> None:
     if instance.path:
         shutil.rmtree(instance.path)
+
+
+@receiver(post_save, sender=TaskResult)
+def task_result_post_save_receiver(
+    sender: Model, instance: TaskResult, created: bool, **kwargs
+) -> None:
+    if instance.name == "django_analyses.node-execution":
+        run_id = instance.result
+        if created and run_id:
+            run = Run.objects.get(id=run_id)
+            run.task_result = instance
+            run.save()
