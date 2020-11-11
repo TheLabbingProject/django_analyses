1f49cfb10f0ba47e5e19832e00c5868ab5ddac20
Zvi Baratz <z.baratz@gmail.com>
2020-11-11 18:59:57 +0200

    Updated setup.py to enable git installed dependencies and omitted .tox directory from .coveragerc.

--- setup.py
+++ setup.py
@@ -5,6 +5,16 @@ with open("README.md", "r") as fh:
 
 with open("requirements.txt") as fh:
     requirements = fh.read().splitlines()
+    install_requires = [
+        requirement
+        for requirement in requirements
+        if not requirement.startswith("git+")
+    ]
+    dependency_links = [
+        requirement
+        for requirement in requirements
+        if requirement.startswith("git+")
+    ]
 
 with open("requirements-dev.txt") as fh:
     dev_requirements = fh.read().splitlines()
